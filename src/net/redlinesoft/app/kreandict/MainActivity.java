8e21739d03a4c3f3c3465a8e986cdfe77a9ee62e
redlinesoft <anoochit@gmail.com>
2012-10-20 23:07:08 +0700

    initial krean dict

---
+++ src/net/redlinesoft/app/kreandict/MainActivity.java
@@ -0,0 +1,131 @@
+package net.redlinesoft.app.kreandict;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import com.google.ads.*;
+
+public class MainActivity extends Activity {
+	 
+	private AdView adView;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+		// Create the adView
+		adView = new AdView(this, AdSize.BANNER, "a15082bc5732b54");
+		// Lookup your LinearLayout assuming it’s been given
+		// the attribute android:id="@+id/mainLayout"
+		LinearLayout layout = (LinearLayout) findViewById(R.id.mainLayout);
+		// Add the adView to it
+		layout.addView(adView);
+		// Initiate a generic request to load it with an ad
+		adView.loadAd(new AdRequest());
+        
+        final DatabaseHandler myDb = new DatabaseHandler(this);
+                
+        // alertdialog
+        final AlertDialog.Builder dDialog = new AlertDialog.Builder(this);
+
+        // write database if not exist
+        myDb.getWritableDatabase();
+        
+        if (myDb.getTotalRow()<=0) {        	 
+			try {
+				replaceDB();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			 
+        } 
+        
+        // AutoCompleteText
+        final String [] myData = myDb.SelectAllData();
+        final AutoCompleteTextView autoCom = (AutoCompleteTextView)findViewById(R.id.autoCompleteText);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, myData);
+		autoCom.setAdapter(adapter);
+                       
+        // searchButton
+        final Button searchButton = (Button)findViewById(R.id.buttonSearch);  
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {    	    			
+    			if (isNotNullNotEmptyNotWhiteSpaceOnlyByJava(autoCom.getText().toString())){
+    				String arrData[] = myDb.SearchWord(autoCom.getText().toString());
+    				if (arrData!=null) {
+    					TextView titleText = (TextView)findViewById(R.id.textMeaningTitle);
+    					titleText.setText(R.string.result_title);
+    					TextView textMeaning = (TextView)findViewById(R.id.textMeaningText);
+    					textMeaning.setText(arrData[2]);
+    				} else {
+    					dDialog.setTitle(R.string.text_alert);
+            			dDialog.setIcon(android.R.drawable.ic_dialog_alert);
+                    	dDialog.setMessage(R.string.text_data_notfound);
+            			dDialog.setPositiveButton(R.string.button_close, null);
+            			dDialog.show();    	
+            			clearTextResult();
+    				}
+
+    			} else {
+        			clearTextResult();
+    			}
+            }
+        });    
+        
+    }
+    
+    public static boolean isNotNullNotEmptyNotWhiteSpaceOnlyByJava(final String string) {  
+    	return string != null && !string.isEmpty() && !string.trim().isEmpty();  
+	}  
+    
+    public void clearTextResult() {
+    	TextView titleText = (TextView)findViewById(R.id.textMeaningTitle);
+		titleText.setText("");
+		TextView textMeaning = (TextView)findViewById(R.id.textMeaningText);
+		textMeaning.setText("");    	
+    }
+    
+    public void replaceDB() throws Exception {    	    	
+    	String FICHIER_BLOW = "teenword.db";
+    	File f=new File(getDataDir().toString(), FICHIER_BLOW);
+    	
+    	BufferedOutputStream bufEcrivain = new BufferedOutputStream((new FileOutputStream(f)));
+    	BufferedInputStream VideoReader = new BufferedInputStream(getResources().openRawResource(R.raw.teenword));
+    	byte[] buff = new byte[32 * 1024];
+    	int len;
+    	while( (len = VideoReader.read(buff)) > 0 ){
+    	    bufEcrivain.write(buff,0,len);
+    	    Log.d("FILE","move database to external storage");
+    	}
+    	bufEcrivain.flush();
+    	bufEcrivain.close();
+    }
+    
+    public String getDataDir() {    	
+        return this.getExternalFilesDir(null).toString();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+ 
+ 
+}
+
+
