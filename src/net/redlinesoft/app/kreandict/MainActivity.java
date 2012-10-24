ec1b82faa20cbd3079182fe3477faf4bacd7a35c
redlinesoft <anoochit@gmail.com>
2012-10-24 22:39:34 +0700

    add TTS feature

--- src/net/redlinesoft/app/kreandict/MainActivity.java
+++ src/net/redlinesoft/app/kreandict/MainActivity.java
@@ -6,10 +6,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.util.Arrays;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,8 +26,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import com.google.ads.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnInitListener {
 
+	private TextToSpeech myTts;
 	private AdView adView;
 
 	@Override
@@ -40,14 +46,20 @@ public class MainActivity extends Activity {
 		// Initiate a generic request to load it with an ad
 		adView.loadAd(new AdRequest());
 
+		// initial database handler
 		final DatabaseHandler myDb = new DatabaseHandler(this);
 
 		// alert dialog
 		final AlertDialog.Builder dDialog = new AlertDialog.Builder(this);
+		final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+
+		// initial Text to Speech
+		myTts = new TextToSpeech(this, this);
 
 		// write database if not exist
 		myDb.getWritableDatabase();
 
+		// check blank database replace it from raw
 		if (myDb.getTotalRow() <= 0) {
 			try {
 				replaceDB();
@@ -63,7 +75,7 @@ public class MainActivity extends Activity {
 		Arrays.sort(myData);
 		final AutoCompleteTextView autoCom = (AutoCompleteTextView) findViewById(R.id.autoCompleteText);
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_dropdown_item_1line,myData);
+				android.R.layout.simple_dropdown_item_1line, myData);
 		autoCom.setAdapter(adapter);
 
 		// searchButton
@@ -80,10 +92,11 @@ public class MainActivity extends Activity {
 								+ getString(R.string.result_title));
 						TextView textMeaning = (TextView) findViewById(R.id.textMeaningText);
 						textMeaning.setText(arrData[2]);
-					} else {
+						Button buttonSpeak = (Button) findViewById(R.id.bottonSpeak);
+						buttonSpeak.setVisibility(1);
+					} else {						
 						dDialog.setTitle(R.string.text_alert);
-						dDialog.setIcon(android.R.drawable.ic_dialog_alert);
-						dDialog.setMessage(R.string.text_data_notfound);
+ 						dDialog.setMessage(R.string.text_data_notfound);
 						dDialog.setPositiveButton(R.string.button_close, null);
 						dDialog.show();
 						clearTextResult();
@@ -91,6 +104,38 @@ public class MainActivity extends Activity {
 
 				} else {
 					clearTextResult();
+				}
+			}
+		});
+
+		// searchButton
+		final Button speakButton = (Button) findViewById(R.id.bottonSpeak);
+		speakButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// check Vaja TTS Engine is exists
+				if (appInstalledOrNot("com.spt.tts.vaja")) {
+					Log.d("PACKAGE", "Vaja is exists");
+					// speak
+					myTts.speak(autoCom.getText().toString(),TextToSpeech.QUEUE_FLUSH, null);
+				} else {
+					Log.d("PACKAGE", "Vaja is not exists, please install first");
+					// asking for install Vaja					
+					adb.setTitle(R.string.text_title_install_tts);
+					adb.setMessage(R.string.text_install_tts);
+					adb.setPositiveButton(R.string.button_yes,
+							new AlertDialog.OnClickListener() {
+								public void onClick(DialogInterface dialog,
+										int arg1) {
+									// OK Event
+									Intent intent = new Intent(
+											Intent.ACTION_VIEW);
+									intent.setData(Uri
+											.parse("market://details?id=com.spt.tts.vaja"));
+									startActivity(intent);
+								}
+							});
+					adb.setNegativeButton(R.string.button_no,null);
+					adb.show();
 				}
 			}
 		});
@@ -107,16 +152,17 @@ public class MainActivity extends Activity {
 		titleText.setText("");
 		TextView textMeaning = (TextView) findViewById(R.id.textMeaningText);
 		textMeaning.setText("");
+		Button buttonSpeak = (Button) findViewById(R.id.bottonSpeak);
+		buttonSpeak.setVisibility(-1);
 	}
 
 	public void replaceDB() throws Exception {
 		String FICHIER_BLOW = "teenword.db";
 		File f = new File(getDataDir().toString(), FICHIER_BLOW);
-
 		BufferedOutputStream bufEcrivain = new BufferedOutputStream(
 				(new FileOutputStream(f)));
 		BufferedInputStream VideoReader = new BufferedInputStream(
-				getResources().openRawResource(R.raw.teenword));
+				getResources().openRawResource(R.raw.teenword));		
 		byte[] buff = new byte[32 * 1024];
 		int len;
 		while ((len = VideoReader.read(buff)) > 0) {
@@ -131,17 +177,41 @@ public class MainActivity extends Activity {
 		return this.getExternalFilesDir(null).toString();
 	}
 
+	private boolean appInstalledOrNot(String uri) {
+		PackageManager pm = getPackageManager();
+		boolean app_installed = false;
+		try {
+			pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+			app_installed = true;
+		} catch (PackageManager.NameNotFoundException e) {
+			app_installed = false;
+		}
+		return app_installed;
+	}
+
+	@Override
+	public void onInit(int status) {
+		//
+	}
+
+	@Override
+	public void onDestroy() {
+		if (myTts != null) {
+			myTts.stop();
+			myTts.shutdown();
+		}
+		super.onDestroy();
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
-
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
-
 		switch (item.getItemId()) {
 		case R.id.menu_updatedb:
 			Log.d("MENU", "select menu update");
@@ -155,10 +225,8 @@ public class MainActivity extends Activity {
 					AboutActivity.class);
 			startActivity(newActivity2);
 			break;
-
 		}
 		return false;
-
 	}
 
 }
